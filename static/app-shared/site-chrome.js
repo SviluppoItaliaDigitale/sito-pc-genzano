52849f114a120a0bd6312057cac11e0f6553b37a
SviluppoItaliaDigitale <clllsn83l26d972f@gmail.com>
2026-05-24 17:41:02 +0200

    Footer: aggiunto link 'Open Data' (hugo.toml + site-chrome.js in coerenza)
    Voce 'Open Data' nel footer 'Link Utili', tra Stato del Sito e Feed RSS. Aggiornati entrambi i punti (menu Hugo e chrome statico) per evitare drift.

--- static/app-shared/site-chrome.js
+++ static/app-shared/site-chrome.js
@@ -226,6 +226,7 @@
                 '<li role="listitem"><a class="list-item" href="' + SITE_URL + '/siti-utili/" style="color:rgba(255,255,255,0.85);">Siti Utili</a></li>' +
                 '<li role="listitem"><a class="list-item" href="' + SITE_URL + '/mappa-sito/" style="color:rgba(255,255,255,0.85);">Mappa del Sito</a></li>' +
                 '<li role="listitem"><a class="list-item" href="' + SITE_URL + '/stato-sistema/" style="color:rgba(255,255,255,0.85);">Stato del Sito</a></li>' +
+                '<li role="listitem"><a class="list-item" href="' + SITE_URL + '/open-data/" style="color:rgba(255,255,255,0.85);">Open Data</a></li>' +
                 '<li role="listitem"><a class="list-item" href="' + SITE_URL + '/feed-rss/" style="color:rgba(255,255,255,0.85);">Feed RSS</a></li>' +
               '</ul></nav>' +
               '<div class="mt-3">' +
